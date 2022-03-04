2d6065f46b6726757348b949caad93fde493097f
marePZS <markopzs@yahoo.com>
2022-03-04 11:22:43 +0100

    added negative tests

--- cypress/integration/registration.spec.js
+++ cypress/integration/registration.spec.js
@@ -8,6 +8,56 @@ describe('register user', ()=>{
 
     it('click on register button', ()=>{
         cy.get('.nav-link').eq(2).click();
+    });
+
+    // it('valid registration', ()=>{           // ------->  valid only once
+    //     cy.get('#first-name').type('Marko');
+    //     cy.get('#last-name').type('Pzs');
+    //     cy.get('#email').type('markopzs5@test.com');
+    //     cy.get('#password').type('password123');
+    //     cy.get('#password-confirmation').type('password123');
+    //     cy.get('[type="checkbox"]').check();
+    //     cy.get('button').click();
+    // });
+
+    it('without first-name', ()=>{ 
+        cy.visit('/register');
+        cy.get('#first-name');
+        cy.get('#last-name').type('Pzs');
+        cy.get('#email').type('markopzs3@test.com');
+        cy.get('#password').type('password123');
+        cy.get('#password-confirmation').type('password123');
+        cy.get('[type="checkbox"]').check();
+        cy.get('button').click();
+        cy.get('#first-name').should('be.empty');
+    });
+
+    it('without last-name', ()=>{ 
+        cy.visit('/register');
+        cy.get('#first-name').type('Marko');
+        cy.get('#last-name');
+        cy.get('#email').type('markopzs3@test.com');
+        cy.get('#password').type('password123');
+        cy.get('#password-confirmation').type('password123');
+        cy.get('[type="checkbox"]').check();
+        cy.get('button').click();
+        cy.get('#last-name').should('be.empty');
+    });
+
+    it('invalid email', ()=>{
+        cy.visit('/register');
+        cy.get('#first-name').type('Marko');
+        cy.get('#last-name').type('Pzs');
+        cy.get('#email').type('markopzs3@testcom');
+        cy.get('#password').type('password123');
+        cy.get('#password-confirmation').type('password123');
+        cy.get('[type="checkbox"]').check();
+        cy.get('button').click();
+        cy.get('.alert').should('have.class', 'alert');
+    });
+    
+    it('already used email', ()=>{
+        cy.visit('/register');
         cy.get('#first-name').type('Marko');
         cy.get('#last-name').type('Pzs');
         cy.get('#email').type('markopzs3@test.com');
@@ -15,5 +65,18 @@ describe('register user', ()=>{
         cy.get('#password-confirmation').type('password123');
         cy.get('[type="checkbox"]').check();
         cy.get('button').click();
+        cy.get('.alert').should('have.class', 'alert'); 
+    });
+
+    it.only('password-confirmation invalid', ()=>{
+        cy.visit('/register');
+        cy.get('#first-name').type('Marko');
+        cy.get('#last-name').type('Pzs');
+        cy.get('#email').type('markopzs3@test.com');
+        cy.get('#password').type('password123');
+        cy.get('#password-confirmation').type('password12333');
+        cy.get('[type="checkbox"]').check();
+        cy.get('button').click();
+        cy.get('.alert').should('have.class', 'alert'); 
     });
 });
